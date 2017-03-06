53157af48b6ed4b3a1ab7d676c176a7a4af441d6
Scott Guyton <kiljoy001@gmail.com>
2017-03-06 12:56:43 -0800

    init commit

--- Models/photoInit.cs
+++ Models/photoInit.cs
@@ -7,15 +7,15 @@ using System.Web;
 
 namespace mvc_demo.Models
 {//DropCreateDatabaseAlways recreates db for the first time. Context is the name of the db
-    public class photoInit: DropCreateDatabaseAlways<photoShareContext>
+    public class photoInit : DropCreateDatabaseAlways<photoShareContext>
     {
-            //This method puts sample data into the database
-            protected override void Seed(photoShareContext context)
-            {
-                base.Seed(context);
+        //This method puts sample data into the database
+        protected override void Seed(photoShareContext context)
+        {
+            base.Seed(context);
 
-                //Create some photos
-                var photos = new List<Photo>
+            //Create some photos
+            var photos = new List<Photo>
             {
                 new Photo {
                     Title = "A Lovely Flower",
@@ -42,11 +42,11 @@ namespace mvc_demo.Models
                     CreatedDate = DateTime.Today
                 }
             }; //add to database
-                photos.ForEach(s => context.Photos.Add(s));
-                context.SaveChanges();
+            photos.ForEach(s => context.Photos.Add(s));
+            context.SaveChanges();
 
-                //Create some comments
-                var comments = new List<Comment>
+            //Create some comments
+            var comments = new List<Comment>
             {
                 new Comment {
                     PhotoID = 1,
@@ -67,23 +67,23 @@ namespace mvc_demo.Models
                     Body = "Wow, that looks great!"
                 }
             };
-                comments.ForEach(s => context.Comments.Add(s));
-                context.SaveChanges();
-            }
-
-            //This gets a byte array for a file at the path specified
-            //The path is relative to the route of the web site
-            //It is used to seed images
-            private byte[] getFileBytes(string path)
-            {
-                FileStream fileOnDisk = new FileStream(HttpRuntime.AppDomainAppPath + path, FileMode.Open);
-                byte[] fileBytes;
-                using (BinaryReader br = new BinaryReader(fileOnDisk))
-                {
-                    fileBytes = br.ReadBytes((int)fileOnDisk.Length);
-                }
-                return fileBytes;
-            }
-
+            comments.ForEach(s => context.Comments.Add(s));
+            context.SaveChanges();
         }
+
+        //This gets a byte array for a file at the path specified
+        //The path is relative to the route of the web site
+        //It is used to seed images
+        private byte[] getFileBytes(string path)
+        {
+            FileStream fileOnDisk = new FileStream(HttpRuntime.AppDomainAppPath + path, FileMode.Open);
+            byte[] fileBytes;
+            using (BinaryReader br = new BinaryReader(fileOnDisk))
+            {
+                fileBytes = br.ReadBytes((int)fileOnDisk.Length);
+            }
+            return fileBytes;
+        }
+
     }
+}
